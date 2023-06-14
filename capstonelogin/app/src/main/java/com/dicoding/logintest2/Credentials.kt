9eaca75b8161720d57495f39d4429af49de1a6bc
Fatinls <91710776+Fatinls@users.noreply.github.com>
2023-06-14 10:12:52 +0700

    Delete capstonelogin directory

--- capstonelogin/app/src/main/java/com/dicoding/logintest2/Credentials.kt
+++
@@ -1,6 +0,0 @@
-package com.dicoding.logintest2
-
-data class Credentials(
-    val username: String,
-    val password: String
-)
--- capstonelogin/app/src/main/java/com/dicoding/logintest2/User.kt
+++
@@ -1,8 +0,0 @@
-package com.dicoding.logintest2
-
-data class User(
-    val username: String,
-    val fullName: String,
-    val email: String,
-    val password: String
-)
